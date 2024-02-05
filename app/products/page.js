5a9501d61b43af935b4b66ca7985c6708dd440c0
Shalini Shukla <shalini.shukla@myccmg.com>
2024-02-05 18:59:47 +0530

    navigation

--- app/products/page.js
+++ app/products/page.js
@@ -1,12 +1,26 @@
+import Link from "next/link";
+
 export default function ProductList() {
+  const productId = 100;
   return (
     <div>
+      <Link href="/">Home</Link>
+
       <h1>Product List</h1>
       <ul>
         <li>
-          <h3>Product 1</h3>
-          <h3>Product 2</h3>
-          <h3>Product 3</h3>
+          <h3>
+            <Link href="./products/1">Product 1</Link>
+          </h3>
+          <h3>
+            <Link href="./products/2">Product 2</Link>
+          </h3>
+          <h3>
+            <Link href="./products/3">Product 3</Link>
+          </h3>
+          <h3>
+            <Link href={`./products/${productId}`}>Product 100</Link>
+          </h3>
         </li>
       </ul>
     </div>
