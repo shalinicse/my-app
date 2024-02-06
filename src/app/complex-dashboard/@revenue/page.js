30993b895c961f6509874b2760a9aaea2fac24f8
Shalini Shukla <shalini.shukla@myccmg.com>
2024-02-06 19:32:25 +0530

    parallel routing

---
+++ src/app/complex-dashboard/@revenue/page.js
@@ -0,0 +1,5 @@
+import Cards from "@/src/components/Cards";
+
+export default function Revenue() {
+  return <Cards>Revenue</Cards>;
+}
---
+++ src/app/f1/(.)f2/page.js
@@ -0,0 +1,8 @@
+export default function InterceptedFile() {
+  return (
+    <div>
+      {/* display a separate UI when page link is clicked  */}
+      <h1>(.) Intercepted File</h1>
+    </div>
+  );
+}
